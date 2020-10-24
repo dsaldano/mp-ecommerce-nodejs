3813d5b05d15e8e635bba14d857a7216bfa754ad
Diego Saldano <dsaldano@preminens.com.ar>
2020-10-24 18:31:10 -0300

    deploy

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require('express')
 var exphbs  = require('express-handlebars');
 const app = express()
-const port = 3000
+//const port = 3000
+var port = process.env.PORT || 3000;
  
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
@@ -18,8 +19,6 @@ app.use(express.static('assets'));
  
 app.use('/assets', express.static(__dirname + '/assets'));
  
-app.listen('/');
-
-/* app.listen(port, () => {
+app.listen(port,() => {
   console.log(`Example app listening at http://localhost:${port}`)
-}) */
+});
